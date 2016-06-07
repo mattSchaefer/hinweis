9172f4ecff655a8297b642ff7d8201cb168d67ab
Matt Schaefer <matthew.schaefer12@gmail.com>
2016-06-07 18:49:58 +0000

    developed a general theme and did some other stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
     def home
     end
+    def adjective_endings
+    end
+    def basic
+    end
+    def about
+    end
    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  get '/about' => 'pages#about'
+  get '/adjective_endings' => 'pages#adjective_endings'
+  get '/basic' => 'pages#basic'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
